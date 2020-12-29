556ac75ffc618e56c5be2f28d7a1b94dc00286e0
Arda Altunyay <ardaltunyay@gmail.com>
2020-12-29 21:08:15 +0300

    Implement navigation component

---
+++ app/src/main/java/com/github/spacedelivery/androidapp/ui/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.github.spacedelivery.androidapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.github.spacedelivery.androidapp.R
+import com.github.spacedelivery.androidapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy(LazyThreadSafetyMode.NONE) {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
+    private val navigationInvisibleScreens = listOf(
+        R.id.createSpaceVehicleFragment
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.bnvMain.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        navController.addOnDestinationChangedListener(this@MainActivity)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        navController.removeOnDestinationChangedListener(this@MainActivity)
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        with(binding.bnvMain) {
+            visibility = when (destination.id) {
+                in navigationInvisibleScreens -> View.GONE
+                else -> View.VISIBLE
+            }
+        }
+    }
+}
