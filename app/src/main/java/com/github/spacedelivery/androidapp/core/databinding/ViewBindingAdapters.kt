2a45f2fc37c45153bc0c1520336d2bd6b101c30c
Arda Altunyay <ardaltunyay@gmail.com>
2020-12-29 21:54:50 +0300

    Create ViewBindingAdapters class

---
+++ app/src/main/java/com/github/spacedelivery/androidapp/core/databinding/ViewBindingAdapters.kt
@@ -0,0 +1,14 @@
+package com.github.spacedelivery.androidapp.core.databinding
+
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+
+object ViewBindingAdapters {
+
+    @JvmStatic
+    @BindingAdapter("visibleWhen")
+    fun getVisibleWhen(view: View, isVisible: Boolean?) {
+        view.isVisible = isVisible ?: false
+    }
+}
