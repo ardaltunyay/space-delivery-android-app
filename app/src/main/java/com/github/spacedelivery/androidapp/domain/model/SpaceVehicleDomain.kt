5033c73cf488a2a648850690c69ce20cfd86f834
Arda Altunyay <ardaltunyay@gmail.com>
2020-12-31 14:18:47 +0300

    Update SpaceVehicleDomain

--- app/src/main/java/com/github/spacedelivery/androidapp/domain/model/SpaceVehicleDomain.kt
+++ app/src/main/java/com/github/spacedelivery/androidapp/domain/model/SpaceVehicleDomain.kt
@@ -7,4 +7,10 @@ data class SpaceVehicleDomain(
     val speed: Int,
     val strength: Int,
     val materialCapacity: Int
-)
+) {
+    fun getUGS(): Int = materialCapacity * 10000
+
+    fun getEUS(): Int = speed * 20
+
+    fun getDS(): Int = strength * 10000
+}
