23a50ec5ba242041f333d4dc7f62991763634867
Arda Altunyay <ardaltunyay@gmail.com>
2020-12-31 16:40:55 +0300

    Create SpaceStationHolder

---
+++ app/src/main/java/com/github/spacedelivery/androidapp/ui/home/adapter/viewholder/SpaceStationHolder.kt
@@ -0,0 +1,25 @@
+package com.github.spacedelivery.androidapp.ui.home.adapter.viewholder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.spacedelivery.androidapp.databinding.ListItemSpaceStationBinding
+import com.github.spacedelivery.androidapp.ui.home.model.SpaceStationUIModel
+
+class SpaceStationHolder(private val binding: ListItemSpaceStationBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    companion object {
+        fun createHolder(parent: ViewGroup): SpaceStationHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ListItemSpaceStationBinding.inflate(inflater, parent, false)
+            return SpaceStationHolder(binding)
+        }
+    }
+
+    fun bind(spaceStationUIModel: SpaceStationUIModel) {
+        with(binding) {
+            this.spaceStationUIModel = spaceStationUIModel
+        }
+    }
+}
