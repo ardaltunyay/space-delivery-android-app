c3ffd8d2962f2be567f561fc616e3e29e6ad971a
Arda Altunyay <ardaltunyay@gmail.com>
2020-12-31 14:13:02 +0300

    Update AppDatabase and RoomDatabaseProvider

--- app/src/main/java/com/github/spacedelivery/androidapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/github/spacedelivery/androidapp/data/local/AppDatabase.kt
@@ -2,10 +2,13 @@ package com.github.spacedelivery.androidapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.github.spacedelivery.androidapp.data.local.dao.SpaceVehicleDao
+import com.github.spacedelivery.androidapp.data.local.dao.ISpaceStationDao
+import com.github.spacedelivery.androidapp.data.local.dao.ISpaceVehicleDao
+import com.github.spacedelivery.androidapp.data.local.entity.SpaceStationEntity
 import com.github.spacedelivery.androidapp.data.local.entity.SpaceVehicleEntity
 
-@Database(entities = [SpaceVehicleEntity::class], version = 1)
+@Database(entities = [SpaceVehicleEntity::class, SpaceStationEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun spaceVehicleDao(): SpaceVehicleDao
+    abstract fun spaceVehicleDao(): ISpaceVehicleDao
+    abstract fun spaceStationDao(): ISpaceStationDao
 }
